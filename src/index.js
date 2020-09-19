4750720d4645af5a84ca07b1ab9c40532ee89f52
Isac Araujo <isac.gaiao@gmail.com>
2020-09-19 04:06:29 -0300

    making better

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+const AWS = require('aws-sdk');
+
+const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({
+	endpoint: process.env.WEBSOCKET_ENDPOINT,
+});
 
 const app = express();
 
@@ -8,10 +14,75 @@ app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.post('/events', async (req, res) => {
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		websocketUri: `${process.env.WEBSOCKET_ENDPOINT}/Prod`
+	});
+});
+
+const connections = [];
+
+app.post('/connected', async (req, res) => {
+	console.log('POST /connected');
 	console.log(new Date(), '>>> Headers: ', req.headers, ' >>> ', req.body);
+
+	const { connectionid } = req.headers;
+	connections.push(connectionid);
 	res.status(200)
 		.send({connected: true});
+});
+
+app.post('/disconnected', async (req, res) => {
+	console.log('POST /disconnected');
+	console.log(new Date(), '>>> Headers: ', req.headers, ' >>> ', req.body);
+
+	const { connectionid } = req.headers;
+	const index = connections.indexOf(connectionid);
+	if (index > -1) {
+		connections.splice(index, 1);
+		console.log('removed from conections');
+	}
+
+	res.status(200)
+		.send({disconnected: true});
+});
+
+app.post('/sendmessage', async (req, res) => {
+	console.log('POST /sendmessage');
+	console.log(new Date(), '>>> Headers: ', req.headers, ' >>> ', req.body);
+
+	const { connectionid } = req.headers;
+
+	const payload = Buffer.from(req.body.data, 'utf8');
+
+	const promises = connections
+		.filter((conn) => conn !== connectionid)
+		.map((conn => {
+			return new Promise((resolve) => {
+				apigatewaymanagementapi.postToConnection({
+					ConnectionId: conn,
+					Data: payload,
+				}, (err, ret) => {
+					if (err) {
+						console.error(err);
+						resolve();
+						return;
+					}
+					console.info(ret);
+					resolve();
+				});
+			});
+		}));
+
+	if (promises.length > 0) {
+		await Promise.all(promises);
+	}
+
+	res.status(200)
+		.send({sendmessage: true});
 });
 
 app.listen(3000, () => {
